a5f752435aed6e7ab18f1d51da00bf755acb5784
Henry <mbahenryemeka@gmail.com>
2024-08-21 23:37:09 -0400

    update setDisplayModal functin to return PhotoData

--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -3,15 +3,19 @@ import PhotoFavButton from './PhotoFavButton';
 import "../styles/PhotoListItem.scss";
 
 
-
-
 const PhotoListItem = (props) => {
-  const { id, location, imageSource, username, profile, addPhoto,setDisplayModal} = props;  
+  const { id, location, imageSource, username, profile, addPhoto,setDisplayModal, photoData} = props;
+  
+  
+  const displayModal = () => {
+    setDisplayModal(photoData)   
+  };
+
 
   return (
     <div className="photo-list__item" id={id}>
       <PhotoFavButton addPhoto={addPhoto} id={id} />
-      <img className="photo-list__image" src={imageSource} onClick={() => setDisplayModal(true) } alt="" />
+      <img className="photo-list__image" src={imageSource} onClick={() => displayModal()} alt="" />
       <div className="photo-list__user-details" >
         <div><img className="photo-list__user-profile" src={profile} alt="" /></div>
         <div className="photo-list__user-info">
