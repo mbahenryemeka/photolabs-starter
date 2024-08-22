71521fb6a96237575275a2f24fbb01e1585e684d
Henry <mbahenryemeka@gmail.com>
2024-08-22 17:03:36 -0400

    add similar photos

--- frontend/src/routes/PhotoDetailsModal.jsx
+++ frontend/src/routes/PhotoDetailsModal.jsx
@@ -2,16 +2,40 @@ import React from 'react';
 
 import '../styles/PhotoDetailsModal.scss'
 import closeSymbol from '../assets/closeSymbol.svg';
-import PhotoListItem from '../components/PhotoListItem';
+import PhotoList from '../components/PhotoList';
+import userEvent from '@testing-library/user-event';
 
-const PhotoDetailsModal = ({setDisplayModal,displayModal}) => {
-  console.log('photo', displayModal);
+const PhotoDetailsModal = ({ setDisplayModal, displayModal }) => {
+  const { urls, user, location, similar_photos } = displayModal;
+  
+  const photos = Object.values(similar_photos);
+  
+
   return (
-    <div className="photo-details-modal">      
-      <button className="photo-details-modal__close-button" onClick={()=>setDisplayModal(false)}>
-        
+    <div className="photo-details-modal">
+      <button className="photo-details-modal__close-button" onClick={() => setDisplayModal(false)}>
         <img src={closeSymbol} alt="close symbol" />
       </button>
+      <div>
+        <img className="photo-details-modal__image" src={urls.full} alt="selected" />
+      </div>
+      <div><img className="photo-list__user-profile" src={user.profile} alt="" /></div>
+      <div className="photo-list__user-info">
+        <p>{user.username}</p>
+        <p className="photo-list__user-location">{location.city}, {location.country}</p>
+      </div>
+      <div>
+        <p>Similar Photos</p>
+      </div>
+      <div>
+      <PhotoList
+        photos={photos}               
+      />
+      </div>
+
+
+
+
     </div>
   )
 };
