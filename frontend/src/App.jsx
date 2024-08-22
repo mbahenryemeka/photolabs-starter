18a9947890afdc1bfce501444163a544f6f9b69a
Henry <mbahenryemeka@gmail.com>
2024-08-21 21:37:31 -0400

    useState for displayModal and setDisplayModal

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,18 @@
-import React from 'react';
+import React,{ useState } from 'react';
 import './App.scss';
 import HomeRoute from './routes/HomeRoute';
 import photos from './mocks/photos';
 import topics from './mocks/topics';
+import PhotoDetailsModal from 'routes/PhotoDetailsModal';
+
 
 const App = () => {
-  
+  const [displayModal, setDisplayModal] = useState(false)
   return (
     <div className="App">
-      <HomeRoute photos={photos} topics={topics} />
+      <HomeRoute photos={photos} topics={topics}  setDisplayModal={setDisplayModal}/>
+      {/* if displayModal is true, render PhotoDetailsModal */}
+      {displayModal && <PhotoDetailsModal/>} 
     </div>
   );
 };
