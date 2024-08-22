92406c875ba9b5bfd7f00a361583d8ae26859cef
Henry <mbahenryemeka@gmail.com>
2024-08-21 23:38:02 -0400

    git the props for photoData object

--- frontend/src/routes/PhotoDetailsModal.jsx
+++ frontend/src/routes/PhotoDetailsModal.jsx
@@ -2,11 +2,14 @@ import React from 'react';
 
 import '../styles/PhotoDetailsModal.scss'
 import closeSymbol from '../assets/closeSymbol.svg';
+import PhotoListItem from '../components/PhotoListItem';
 
-const PhotoDetailsModal = ({setDisplayModal}) => {
+const PhotoDetailsModal = ({setDisplayModal,displayModal}) => {
+  
   return (
-    <div className="photo-details-modal">
+    <div className="photo-details-modal">      
       <button className="photo-details-modal__close-button" onClick={()=>setDisplayModal(false)}>
+        
         <img src={closeSymbol} alt="close symbol" />
       </button>
     </div>
