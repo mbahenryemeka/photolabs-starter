8c40c47974d72b96295e9aa8df0d97d42198b5ce
Henry <mbahenryemeka@gmail.com>
2024-08-22 01:01:10 -0400

    update photoDetailsModal.jsx

--- frontend/src/routes/PhotoDetailsModal.jsx
+++ frontend/src/routes/PhotoDetailsModal.jsx
@@ -5,7 +5,7 @@ import closeSymbol from '../assets/closeSymbol.svg';
 import PhotoListItem from '../components/PhotoListItem';
 
 const PhotoDetailsModal = ({setDisplayModal,displayModal}) => {
-  
+  console.log('photo', displayModal);
   return (
     <div className="photo-details-modal">      
       <button className="photo-details-modal__close-button" onClick={()=>setDisplayModal(false)}>
