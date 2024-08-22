6c3e8e8b85f488cf526d755c5948a63fa27b6963
Henry <mbahenryemeka@gmail.com>
2024-08-21 22:15:32 -0400

    Add close button to the modal

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,12 +7,14 @@ import PhotoDetailsModal from 'routes/PhotoDetailsModal';
 
 
 const App = () => {
-  const [displayModal, setDisplayModal] = useState(false)
+  const [displayModal, setDisplayModal] = useState(false);
+  
+
   return (
     <div className="App">
-      <HomeRoute photos={photos} topics={topics}  setDisplayModal={setDisplayModal}/>
+     <HomeRoute photos={photos} topics={topics}  setDisplayModal={setDisplayModal}/>
       {/* if displayModal is true, render PhotoDetailsModal */}
-      {displayModal && <PhotoDetailsModal/>} 
+      {displayModal && <PhotoDetailsModal setDisplayModal={setDisplayModal}/>} 
     </div>
   );
 };
